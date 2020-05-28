613d6dc5f8864bd41660a9ac4e147e26dc150cd0
Owner <Owner@AF393>
2020-05-28 09:49:05 +0900

    テスト

--- src/shtjava/InsertTest.java
+++ src/shtjava/InsertTest.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class InsertTest {
 
 	public static void main(String[] args) {
-
+// 繁田さんテスト
 		try {
 			// JDBCドライバの登録
 			Class.forName("com.mysql.cj.jdbc.Driver");
